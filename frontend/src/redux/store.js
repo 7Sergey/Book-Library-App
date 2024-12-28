284c4bdd90a7eff7237f339ba97ea1185175b2e3
Sergey Fedorov <sergey14fedorov@gmail.com>
2024-12-28 22:48:37 +0300

    added reducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import booksReduser from './books/reducer';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    books: booksReduser,
+  },
 });
 
 export default store;
