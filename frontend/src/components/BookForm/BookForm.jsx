260cfbc817354dd3c326734b50f62d1107977ca4
Sergey Fedorov <sergey14fedorov@gmail.com>
2024-12-30 10:07:15 +0300

    separate function for book creation

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,11 +1,11 @@
 // внешние импорты
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { v4 as uuidv4 } from "uuid";
 // локальные импорты
 import booksData from "../../data/books.json";
 import { addBook } from "../../redux/books/actionCreators";
 import "./BookForm.css";
+import createBookWithId from "../../utils/createBookWithId";
 
 const BookForm = () => {
   const [title, setTitle] = useState("");
@@ -14,26 +14,20 @@ const BookForm = () => {
 
   const handleAddRandomBook = () => {
     const randomIndex = Math.floor(Math.random() * booksData.length);
-
     const randomBook = booksData[randomIndex];
+    const randomBookWithId = createBookWithId(randomBook);
 
-    const randomBookWithId = {
-      ...randomBook,
-      id: uuidv4(),
-      isFavorite: false,
-    };
-    dispatch(addBook(randomBookWithId));
+    dispatch(addBook(createBookWithId(randomBookWithId)));
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (title && author) {
-      const book = {
+      const book = createBookWithId({
         title: title,
         author: author,
-        id: uuidv4(), // генерация id из uuid - генератор id
-        isFavorite: false,
-      };
+      }); //передаем в функцию объект с двумя свойствами
 
       dispatch(addBook(book)); // вызвав addBook мы получим объект с type и peyload. И этот объект передаем в dispatch
 
