e23e92526b8ad52e6d11964b3bb887dbff9b297f
Sergey Fedorov <sergey14fedorov@gmail.com>
2025-01-10 13:51:39 +0300

    input title filter

--- frontend/src/components/Filter/Filter.jsx
+++ frontend/src/components/Filter/Filter.jsx
@@ -1,9 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectTitleFilter,
+  setTitleFilter,
+} from "../../redux/slices/filterSlice";
 import "./Filter.css";
 
 const Filter = () => {
+  const dispatch = useDispatch();
+  const titleFilter = useSelector(selectTitleFilter);
+
+  const handleTitleFilterChange = (e) => {
+    dispatch(setTitleFilter(e.target.value));
+  };
   return (
     <div className="app-block filter">
-      <h2>Filters</h2>
+      <div className="filter-group">
+        <input
+          type="text"
+          placeholder="Filter by title..."
+          onChange={handleTitleFilterChange}
+          value={titleFilter}
+        />
+      </div>
     </div>
   );
 };
