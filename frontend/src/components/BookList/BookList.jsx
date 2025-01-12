6a5ffd0c5c37ab3844c5c2c96cbde3c162c3ef97
Sergey Fedorov <sergey14fedorov@gmail.com>
2025-01-12 09:48:17 +0300

    Highlight filter text matches

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -17,26 +17,50 @@ const BookList = () => {
   const authorFilter = useSelector(selectAuthorFilter);
   const favoriteFilter = useSelector(selectFavoriteFilter);
 
+  //удаление книги
   const handleDelete = (id) => {
     dispatch(deleteBook(id));
   };
 
+  //переключение избранных книг
   const handleToggle = (id) => {
     dispatch(toggleFavorite(id));
   };
 
+  //фильтрация книг
   const filtredBooks = books.filter((book) => {
-    //Преобразовал к нижнему регистру название книги, сравнил её с фильтром пользователя.
+    //Преобразовал к нижнему регистру название книги, сравнил её с фильтром по названию книги.
     const matchesTitle = book.title
       .toLowerCase()
       .includes(titleFilter.toLowerCase());
 
+    //Преобразовал к нижнему регистру название книги, сравнил её с фильтром по автору.
     const matchesAuthor = book.author
       .toLowerCase()
       .includes(authorFilter.toLowerCase());
+
     const matchesFavorite = favoriteFilter ? book.isFavorite : true;
+
     return matchesTitle && matchesAuthor && matchesFavorite;
   });
+
+  const highlightMatch = (text, filter) => {
+    if (!filter) return text;
+
+    const regex = new RegExp(`(${filter})`, "gi");
+
+    return text.split(regex).map((substring, i) => {
+      if (substring.toLowerCase() === filter.toLowerCase()) {
+        return (
+          <span key={i} className="highlight">
+            {substring}
+          </span>
+        );
+      }
+      return substring;
+    });
+  };
+
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -47,7 +71,8 @@ const BookList = () => {
           {filtredBooks.map((book, i) => (
             <li key={book.id}>
               <div className="book-info">
-                {++i}. {book.title} by <strong>{book.author}</strong>{" "}
+                {++i}. {highlightMatch(book.title, titleFilter)} by{" "}
+                <strong>{highlightMatch(book.author, authorFilter)}</strong>{" "}
               </div>
 
               <div className="book-actions">
