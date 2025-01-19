62fddc869c5cfa9b1ae6274492a8d7f23aac4fd8
Sergey Fedorov <sergey14fedorov@gmail.com>
2025-01-19 19:36:28 +0300

    Added rateLimit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+app.use(limiter);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mongo');
 
