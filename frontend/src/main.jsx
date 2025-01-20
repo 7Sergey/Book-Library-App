594e47692c8bc4a61c0d01824b8a24d31d8e1394
Sergey Fedorov <sergey14fedorov@gmail.com>
2025-01-20 11:35:30 +0300

    Added BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,18 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router";
+
 import "./index.css";
 import App from "./App.jsx";
 import store from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
