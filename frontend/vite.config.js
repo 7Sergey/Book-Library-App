20f838df763fd7f02e0bc67635e0514cdd38d31a
Sergey Fedorov <sergey14fedorov@gmail.com>
2025-01-24 14:09:28 +0300

    DEPLOY

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Уберите `external`, если хотите, чтобы зависимость была частью сборки
-    rollupOptions: {
-      // Убедитесь, что `react-redux` не указан как внешняя зависимость
-      // external: ['react-redux'],
-    },
-  },
 });
