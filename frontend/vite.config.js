fdcb724024e4406f364a4da9435cdc8902d91323
Sergey Fedorov <sergey14fedorov@gmail.com>
2025-01-24 15:35:30 +0300

    edit vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  rewrites: [{ source: '/(.*)', destination: '/' }],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 4000,
+  },
 });
